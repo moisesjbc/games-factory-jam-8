25b83c49b4da959fd6b52ae34f40364bb162f291
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-10-26 20:22:21 +0100

    Added energy waste to player movement

--- godot-project/scripts/player.gd
+++ godot-project/scripts/player.gd
@@ -5,19 +5,21 @@ extends RigidBody2D
 # var b = "textvar"
 
 export (int) var impulse = 500;
+export (float) var movement_energy_cost = 0.5;
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-
 func _physics_process(delta):
 	var offset = Vector2(0.0, 0.0)
+	var direction = 0.0
 	if Input.is_action_pressed("ui_left"):
-		apply_impulse(offset, Vector2(-1.0 * impulse * delta, 0.0))
-	if Input.is_action_pressed("ui_right"):
-		apply_impulse(offset, Vector2(1.0 * impulse * delta, 0.0))
+		direction = -1.0
+	elif Input.is_action_pressed("ui_right"):
+		direction = 1.0
+		
+	if direction != 0.0 and $energy_bar.value >= movement_energy_cost:
+		apply_impulse(offset, Vector2(direction * impulse * delta, 0.0))
+		$energy_bar.value -= movement_energy_cost
