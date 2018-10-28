9a5bea50a338e2ffd50cff35fe06f83b4bac8bc7
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-10-28 17:52:33 +0000

    Game over screen improved

--- godot-project/scripts/game_over.gd
+++ godot-project/scripts/game_over.gd
@@ -9,3 +9,9 @@ func hide():
 	visible = false
 	get_tree().paused = false
 	get_tree().change_scene("res://scenes/main.tscn")
+
+
+func _on_return_to_main_menu_button_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://scenes/menus/main_menu.tscn")
+
