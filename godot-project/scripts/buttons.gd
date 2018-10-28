bd89463a690e3a203f14bce5601aae5ad6974525
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-10-28 03:25:22 +0000

    Added 'How to play' screen

--- godot-project/scripts/buttons.gd
+++ godot-project/scripts/buttons.gd
@@ -6,3 +6,7 @@ func _on_play_button_pressed():
 
 func _on_game_over_button_pressed():
 	get_tree().quit()
+
+
+func _on_how_to_play_button_pressed():
+	get_tree().change_scene("res://scenes/menus/how_to_play.tscn")
---
+++ godot-project/scripts/how_to_play.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func _on_return_to_main_menu_buton_pressed():
+	get_tree().change_scene("res://scenes/menus/main_menu.tscn")
